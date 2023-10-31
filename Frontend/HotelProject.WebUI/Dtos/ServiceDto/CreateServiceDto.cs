d6f9c1a2c02b747daad52372bd2cee8beebd4813
serkancan <serkancan@DESKTOP-9CS0Q1K>
2023-10-31 05:04:21 +0300

    Hizmetler sayfasının yapımı

---
+++ Frontend/HotelProject.WebUI/Dtos/ServiceDto/CreateServiceDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.ServiceDto
+{
+    public class CreateServiceDto
+    {
+        [Required(ErrorMessage ="Hİzmet ikon linki giriniz")]
+        public string ServiceIcon { get; set; }
+        [Required(ErrorMessage = "Hİzmet başlıgı giriniz")]
+        [StringLength(100,ErrorMessage = "Hİzmet başlıgı en fazla 100 karakter olabilir.")]
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Frontend/HotelProject.WebUI/Dtos/ServiceDto/UpdateServiceDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.ServiceDto
+{
+    public class UpdateServiceDto
+    {
+        public int ServiceID { get; set; }
+
+        [Required(ErrorMessage = "Hİzmet ikon linki giriniz")]
+        public string ServiceIcon { get; set; }
+        [Required(ErrorMessage = "Hİzmet başlıgı giriniz")]
+        [StringLength(100, ErrorMessage = "Hİzmet başlıgı en fazla 100 karakter olabilir.")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Hİzmet açıklaması giriniz")]
+        [StringLength(100, ErrorMessage = "Hİzmet açıklaması en fazla 100 karakter olabilir.")]
+        public string Description { get; set; }
+    }
+}
