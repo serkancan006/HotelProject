c0037b220001f3c313e51ecc8eaf2c82acd76ee7
serkancan <serkancan@DESKTOP-9CS0Q1K>
2023-11-11 00:52:49 +0300

    Rezervasyon Sayfaları

---
+++ ApiConsume/HotelProject.WebApi/Controllers/BookingController.cs
@@ -0,0 +1,51 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        private readonly IBookingService _bookingBooking;
+
+        public BookingController(IBookingService bookingBooking)
+        {
+            _bookingBooking = bookingBooking;
+        }
+
+        [HttpGet]
+        public IActionResult BookingList()
+        {
+            var values = _bookingBooking.TGetList();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult AddBooking(Booking booking)
+        {
+            _bookingBooking.TInsert(booking);
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBooking(int id)
+        {
+            var values = _bookingBooking.TGetByID(id);
+            _bookingBooking.TDelete(values);
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateBooking(Booking booking)
+        {
+            _bookingBooking.TUpdate(booking);
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetBooking(int id)
+        {
+            var values = _bookingBooking.TGetByID(id);
+            return Ok(values);
+        }
+    }
+}
---
+++ Frontend/HotelProject.WebUI/Controllers/BookingController.cs
@@ -0,0 +1,46 @@
+﻿using HotelProject.WebUI.Dtos.BookingDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class BookingController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public BookingController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public PartialViewResult AddBooking()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddBooking(CreateBookingDto model)
+        {
+            //if (!ModelState.IsValid)
+            //{
+            //    return View();
+            //}
+            model.Status = "Onay Bekliyor";
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44358/api/Booking", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Default");
+            }
+            return View();
+        }
+    }
+}
