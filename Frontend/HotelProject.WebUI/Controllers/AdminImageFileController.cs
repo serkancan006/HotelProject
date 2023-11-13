f54891239a259a0d74bf0e9ec2d5e0d5abcfc52a
serkancan <serkancan@DESKTOP-9CS0Q1K>
2023-11-14 02:21:58 +0300

    Ders 19 sonu api dosya islemleri

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminImageFileController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminImageFileController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile file)
+        {
+            var stream = new MemoryStream();
+            await file.CopyToAsync(stream);
+            var bytes = stream.ToArray();
+
+            ByteArrayContent byteArrayContent = new ByteArrayContent(bytes);
+            byteArrayContent.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);
+            MultipartFormDataContent multipartFormDataContent = new MultipartFormDataContent();
+            multipartFormDataContent.Add(byteArrayContent, "file", file.FileName);
+            var httpClient = new HttpClient();
+            var responseMessage = await httpClient.PostAsync("https://localhost:44358/api/FileImage", multipartFormDataContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return View();
+            }
+
+            return View();
+        }
+    }
+}
