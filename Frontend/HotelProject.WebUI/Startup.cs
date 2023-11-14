83c977cbb28381222bdc331b5dcba0935d5c2b19
serkancan <serkancan@DESKTOP-9CS0Q1K>
2023-11-14 03:52:01 +0300

    bölüm 20 sonu Authorize ve AllowAnonymous

--- Frontend/HotelProject.WebUI/Startup.cs
+++ Frontend/HotelProject.WebUI/Startup.cs
@@ -4,9 +4,11 @@ using HotelProject.DataAccessLayer.Concrete;
 using HotelProject.EntityLayer.Concrete;
 using HotelProject.WebUI.Dtos.GuestDto;
 using HotelProject.WebUI.ValidationRules.GuestValidationRules;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -37,6 +39,20 @@ namespace HotelProject.WebUI
 			services.AddTransient<IValidator<UpdateGuestDto>, UpdateGuestValidator>();
 			services.AddControllersWithViews().AddFluentValidation();
 			services.AddAutoMapper(typeof(Startup));
+			services.AddMvc(config =>
+			{
+				var policy = new AuthorizationPolicyBuilder()
+				.RequireAuthenticatedUser()
+				.Build();
+				config.Filters.Add(new AuthorizeFilter(policy));
+			});
+
+			services.ConfigureApplicationCookie(options =>
+			{
+				options.Cookie.HttpOnly = true;
+				options.ExpireTimeSpan = TimeSpan.FromMinutes(10);
+				options.LoginPath = "/Login/Index/";
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,9 +68,10 @@ namespace HotelProject.WebUI
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
+			app.UseStatusCodePagesWithReExecute("/ErrorPage/Error404", "?0code={0}");
 			app.UseHttpsRedirection();
 			app.UseStaticFiles();
-
+			app.UseAuthentication();
 			app.UseRouting();
 
 			app.UseAuthorization();
