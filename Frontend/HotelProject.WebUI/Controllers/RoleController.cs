49edf9a9b20980c1b9388efedf4c25e42c4c49a8
serkancan <serkancan@DESKTOP-9CS0Q1K>
2023-11-15 20:49:42 +0300

    Bölüm 28 Sonu Rol Atamaları

---
+++ Frontend/HotelProject.WebUI/Controllers/RoleController.cs
@@ -0,0 +1,70 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Models.Role;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<AppRole> roleManager;
+
+        public RoleController(RoleManager<AppRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var values = roleManager.Roles.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddRole(AddRoleViewModel model)
+        {
+            AppRole approle = new AppRole()
+            {
+                Name = model.RoleName
+            };
+            var result = await roleManager.CreateAsync(approle);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+        public async Task<IActionResult> DeleteRole(int id)
+        {
+            var values = roleManager.Roles.FirstOrDefault(x => x.Id == id);
+            await roleManager.DeleteAsync(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateRole(int id)
+        {
+            var value = roleManager.Roles.FirstOrDefault(x => x.Id == id);
+            UpdateRoleViewModel updateRoleViewModel = new UpdateRoleViewModel()
+            {
+                RoleID = value.Id,
+                RoleName = value.Name
+            };
+            return View(updateRoleViewModel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateRole(UpdateRoleViewModel updateRoleViewModel)
+        {
+            var value = roleManager.Roles.FirstOrDefault(x => x.Id == updateRoleViewModel.RoleID);
+            value.Name = updateRoleViewModel.RoleName;
+            await roleManager.UpdateAsync(value);
+            return RedirectToAction("Index");
+        }
+    }
+}
